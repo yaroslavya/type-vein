f3fd8fafb7451dc0a7eecaa1ccae7935ef33e9c9
Magna Marek <magna.marek@pbs.one>
2019-11-20 21:51:43 +0100

    [mod] allow attributes to have value other than "true"

--- src/attribute.ts
+++ src/attribute.ts
@@ -1,8 +1,19 @@
-export type Attribute
-    = "filterable"
-    | "indexable"
-    | "iterable"
-    | "unique";
+export interface Attributes {
+    filterable: true;
+    indexable: true;
+    iterable: {
+        sorted: boolean;
+    };
+    unique: true;
+}
+
+export type Attribute = keyof Attributes;
+export type HasAttribute<A extends Attribute> = Record<A, Attributes[A]>;
+export type IncludesAttribute<T extends string[], A extends Attribute> = undefined extends T ? false : A extends T[number] ? true : false;
+export type IsFilterable = HasAttribute<"filterable">;
+export type IsIndexable = HasAttribute<"indexable">;
+export type IsIterable = HasAttribute<"iterable">;
+export type IsUnique = HasAttribute<"unique">;
 
 const attributesMap: Record<Attribute, true> = {
     filterable: true,
@@ -15,15 +26,8 @@ export function allAttributes(): Attribute[] {
     return Object.keys(attributesMap) as Attribute[];
 }
 
-export type HasAttribute<F extends Attribute> = Record<F, true>;
-
-export type IsFilterable = HasAttribute<"filterable">;
-export type IsIndexable = HasAttribute<"indexable">;
-export type IsIterable = HasAttribute<"iterable">;
-export type IsUnique = HasAttribute<"unique">;
-
-export function setAttribute<T extends object, A extends Attribute>(property: T, attribute: A): T & HasAttribute<A> {
-    (property as any)[attribute] = true;
+export function setAttribute<T extends object, A extends Attribute>(property: T, attribute: A, value: Attributes[A]): T & HasAttribute<A> {
+    (property as any)[attribute] = value;
 
     return property as any;
 }
@@ -31,8 +35,6 @@ export function setAttribute<T extends object, A extends Attribute>(property: T,
 export function hasAttribute<A extends Attribute>(property: any, attribute: A): property is HasAttribute<A> {
     return property?.[attribute] === true;
 }
-
-export type IncludesAttribute<T extends string[], A extends Attribute> = undefined extends T ? false : A extends T[number] ? true : false;
 
 export function includesAttribute<A extends Attribute>(attributes: string[], attribute: A): attributes is A[] {
     return attributes.indexOf(attribute) !== -1;
--- src/property-builder.ts
+++ src/property-builder.ts
@@ -1,7 +1,7 @@
 import { Property } from "./property";
 import { Primitive } from "./lang";
 import { IsLoadable, ContextValue, IncludesContextValue, IsCreatable, IsPatchable, Context, setContext } from "./context";
-import { IsFilterable, IsIterable, IsUnique, IsIndexable, setAttribute } from "./attribute";
+import { IsFilterable, IsIterable, IsUnique, IsIndexable, setAttribute, Attributes } from "./attribute";
 
 export class PropertyBuilder<K extends string, V, P extends Property<K, V> = Property<K, V>> {
     constructor(key: K, value: V) {
@@ -17,45 +17,40 @@ export class PropertyBuilder<K extends string, V, P extends Property<K, V> = Pro
     readonly _property: P;
 
     filterable(): PropertyBuilder<K, V, P & IsFilterable> {
-        setAttribute(this._property, "filterable");
+        setAttribute(this._property, "filterable", true);
         return this as any;
     }
 
     indexable(): PropertyBuilder<K, V, P & IsIndexable> {
-        setAttribute(this._property, "indexable");
+        setAttribute(this._property, "indexable", true);
         return this as any;
     }
 
-    iterable(): PropertyBuilder<K, V, P & IsIterable> {
-        setAttribute(this._property, "iterable");
-
+    iterable(args?: Attributes["iterable"]): PropertyBuilder<K, V, P & IsIterable> {
+        setAttribute(this._property, "iterable", args || { sorted: false });
         return this as any;
     }
 
     unique(): PropertyBuilder<K, V, P & IsUnique> {
-        setAttribute(this._property, "unique");
-
+        setAttribute(this._property, "unique", true);
         return this as any;
     }
 
     creatable<F extends (keyof ContextValue)[] = never[]>(flags?: F)
         : PropertyBuilder<K, V, P & IsCreatable<IncludesContextValue<F, "omittable">, IncludesContextValue<F, "nullable">, IncludesContextValue<F, "voidable">>> {
         setContext(this._property, "creatable", flags);
-
         return this as any;
     }
 
     loadable<F extends (keyof ContextValue)[] = never[]>(flags?: F)
         : PropertyBuilder<K, V, P & IsLoadable<IncludesContextValue<F, "omittable">, IncludesContextValue<F, "nullable">, IncludesContextValue<F, "voidable">>> {
         setContext(this._property, "loadable", flags);
-
         return this as any;
     }
 
     patchable<F extends (keyof ContextValue)[] = never[]>(flags?: F)
         : PropertyBuilder<K, V, P & IsPatchable<IncludesContextValue<F, "omittable">, IncludesContextValue<F, "nullable">, IncludesContextValue<F, "voidable">>> {
         setContext(this._property, "patchable", flags);
-
         return this as any;
     }
 
@@ -63,11 +58,3 @@ export class PropertyBuilder<K extends string, V, P extends Property<K, V> = Pro
         return this._property;
     }
 }
-
-let property: Property<"foo", typeof String> = {
-    key: "foo",
-    primitive: true,
-    value: String
-};
-
-let builder = new PropertyBuilder("foo", String).loadable(["omittable", "nullable"]).creatable()._property;
