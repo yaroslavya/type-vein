b0acfe9c3006af149165abbc1de8136c1104a615
Magna Marek <magna.marek@pbs.one>
2019-12-06 13:13:13 +0100

    [fix] Type selected via TypeSelector should always include voidable properties closes #3

--- src/type-selector.ts
+++ src/type-selector.ts
@@ -5,7 +5,7 @@ import { Context, HasContext, ChangeContextVoidable } from "./context";
 import { SelectionSymbol, Selection } from "./selection";
 import { Select } from "./select";
 
-export class TypeSelector<T extends Type, C extends Context, S = Select<T, HasContext<C, any, false>>> {
+export class TypeSelector<T extends Type, C extends Context, S = Select<T, HasContext<C>>> {
     constructor(type: T, context: C) {
         if (!Type.is(type)) {
             throw new Error(`expected argument 'type' to be a Type`);
@@ -14,44 +14,31 @@ export class TypeSelector<T extends Type, C extends Context, S = Select<T, HasCo
         this._type = type;
         this._context = context;
 
+        /**
+         * [todo] copy over all properties of T
+         */
         let selectedType: Selection = {
             [SelectionSymbol]: Selection.createMetadata(type)
         };
 
         this._selected = selectedType as any as S;
-        this.select(context);
     }
 
     private readonly _type: T;
     private readonly _context: C;
     private readonly _selected: S;
 
-    /**
-     * [todo] should only be called once by ctor and therefore not really exist as a public method
-     */
-    select<C extends Context>(context: C): TypeSelector<T, C, S & Select<T, HasContext<C, any, false>>>;
-
     select<P extends Property.Primitive & HasContext<C, any, true>>(
         select: (properties: T) => P
-    ): TypeSelector<T, C, S & Record<P["key"], ChangeContextVoidable<P, C, false>>>;
+    ): TypeSelector<T, C, Omit<S, P["key"]> & Record<P["key"], ChangeContextVoidable<P, C, false>>>;
 
     select<P extends Property.Complex & HasContext<C, any, any>, E>(
         select: (properties: T) => P,
         expand: (selector: TypeSelector<Unbox<P["value"]>, C>) => TypeSelector<Unbox<P["value"]>, C, E>
-    ): TypeSelector<T, C, S & Record<P["key"], ReplacePropertyValue<ChangeContextVoidable<P, C, false>, E>>>;
+    ): TypeSelector<T, C, Omit<S, P["key"]> & Record<P["key"], ReplacePropertyValue<ChangeContextVoidable<P, C, false>, E>>>;
 
     select(...args: any[]): any {
-        if (args.length === 1 && typeof args[0] === "string") {
-            let properties = propertiesOf(this._type);
-
-            for (let k in properties) {
-                /**
-                 * [todo] only select the ones which have context C (args[0]) and are not omittable,
-                 * also crawl properties with context C of expanded types
-                 */
-                this._selectProperty(properties[k]);
-            }
-        } else if (args.length === 1 && args[0] instanceof Function) {
+        if (args.length === 1 && args[0] instanceof Function) {
             this._selectProperty(this._fetchProperty(args[0]));
         } else if (args.length === 2 && args[0] instanceof Function && args[1] instanceof Function) {
             let property = this._fetchProperty(args[0]);
--- test/unit/type-selector.spec.ts
+++ test/unit/type-selector.spec.ts
@@ -1,4 +1,4 @@
-import { Type, Property, TypeSelector, TypeSymbol, SelectionSymbol, IsIterable } from "../../src";
+import { Type, Property, TypeSelector, TypeSymbol, SelectionSymbol, IsIterable, Instance } from "../../src";
 
 describe("type-selector", () => {
     it("should create a selection as expected", () => {
@@ -7,21 +7,21 @@ describe("type-selector", () => {
          */
         class AlbumType {
             [TypeSymbol] = Type.createMetadata(AlbumType);
-            name = Property.create("name", String);
-            releasedAt = Property.create("releasedAt", String);
-            songs = Property.create("songs", SongType, b => b.iterable());
+            name = Property.create("name", String, b => b.loadable(["voidable"]));
+            releasedAt = Property.create("releasedAt", String, b => b.loadable(["voidable"]));
+            songs = Property.create("songs", SongType, b => b.iterable().loadable());
         }
 
         class SongType {
             [TypeSymbol] = Type.createMetadata(SongType);
-            album = Property.create("album", AlbumType);
-            duration = Property.create("duration", Number);
-            name = Property.create("name", String);
+            album = Property.create("album", AlbumType, b => b.loadable(["voidable"]));
+            duration = Property.create("duration", Number, b => b.loadable(["voidable"]));
+            name = Property.create("name", String, b => b.loadable(["voidable"]));
         }
 
         let sourceType = new AlbumType();
         let songType = new SongType();
-        let typeSelector = new TypeSelector(sourceType);
+        let typeSelector = new TypeSelector(sourceType, "loadable");
 
         /**
          * [act]
@@ -30,7 +30,6 @@ describe("type-selector", () => {
             .select(x => x.name)
             .select(x => x.songs, q => q
                 .select(x => x.name)
-                .select(x => x.duration)
                 .select(x => x.album, q => q
                     .select(x => x.releasedAt)
                 )
@@ -43,12 +42,14 @@ describe("type-selector", () => {
         let sourceMetadata = selectedType[SelectionSymbol].type[TypeSymbol];
 
         expect(sourceMetadata.class).toBe(AlbumType, "source metadata class was expected to be 'AlbumType'");
-        expect(selectedType.name).not.toBe(sourceType.name, "expected property to be cloned: name");
-        expect(selectedType.name).toEqual(sourceType.name, "expected property to equal the one from source: name");
+        expect(selectedType.name).not.toBe(sourceType.name as any, "expected property to be cloned: name");
+        expect(selectedType.name).toEqual(sourceType.name as any, "expected property to equal the one from source: name");
+        expect(selectedType.name.loadable.voidable).toBe(false, "expected name property to no longer be voidable because it was selected");
         expect(selectedType.songs).not.toBe(sourceType.songs as any, "expected property to be cloned: songs");
         expect(selectedType.songs.value instanceof Function).toBe(false, "expanded type was still a class: songs");
-        expect(selectedType.songs.value.duration).not.toBe(songType.duration, "expected property to be cloned: songs.duration");
-        expect(selectedType.songs.value.duration).toEqual(songType.duration, "expected property to equal the one from source:  songs.duration");
+        expect(selectedType.songs.value.duration.loadable.voidable).toBe(true, "expected name property to no longer be voidable because it was selected");
+        expect(selectedType.songs.value.duration).not.toBe(songType.duration as any, "expected property to be cloned: songs.duration");
+        expect(selectedType.songs.value.duration).toEqual(songType.duration as any, "expected property to equal the one from source: songs.duration");
         expect(selectedType.songs.value.album).not.toBe(selectedType as any, "unexpected type recursion: songs.album");
         expect(selectedType.songs.value.album).not.toEqual(selectedType as any, "unexpected type recursion: songs.album");
     });
