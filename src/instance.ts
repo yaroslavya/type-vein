0ff1262df2b0a946629ad5f59d9b7c718faecf50
Magna Marek <magna.marek@pbs.one>
2019-12-06 13:18:44 +0100

    [polish] remove obsolete checks against undefined @ Instance

--- src/instance.ts
+++ src/instance.ts
@@ -5,13 +5,12 @@ import { Context, HasContext, WidenValueForContext } from "./context";
 
 export type BoxPropertyValue<P, V> = P extends IsIterable ? V[] : V;
 
-export type InstancedValueOfProperty<P extends Property | undefined, C extends Context, V = Exclude<P, undefined>["value"]>
-    = undefined extends P
-    ? (V extends Primitive ? BoxPropertyValue<Exclude<P, undefined>, ReturnType<V>> : BoxPropertyValue<Exclude<P, undefined>, Instance<Unbox<V>, C>>) | undefined
-    : V extends Primitive ? BoxPropertyValue<P, ReturnType<V>> : BoxPropertyValue<P, Instance<Unbox<V>, C>>;
+export type InstancedValueOfProperty<P extends Property, C extends Context, X>
+    = P["value"] extends Primitive ? BoxPropertyValue<P, ReturnType<P["value"]>>
+    : BoxPropertyValue<P, Instance<Unbox<P["value"]>, C, X>>;
 
-export type Instance<T, C extends Context> = {
-    [K in PropertyKeys<T, HasContext<C, any, any>>]: WidenValueForContext<T[K], C, InstancedValueOfProperty<T[K], C>>;
+export type Instance<T, C extends Context, P = Property> = {
+    [K in PropertyKeys<T, HasContext<C, any, any> & P>]: WidenValueForContext<T[K], C, InstancedValueOfProperty<T[K], C, P>>;
 };
 
 export type AliasedInstancedValueOfProperty<P extends Property, C extends Context>
