3ebe1482b1ba4d8530ceca949c403b8eda8ce888
Magna Marek <magna.marek@pbs.one>
2019-12-07 02:33:58 +0100

    [doc] added comments for Class, Unbox and Type #10

--- src/lang.ts
+++ src/lang.ts
@@ -1,4 +1,9 @@
 /**
+ * [note] this file contains the most atomic and non-type-vein specific types.
+ * you could consider it to be a universal extension to typescript/lib.d.ts
+ */
+
+/**
  * The union of all native constructors that return a primitive.
  */
 export type Primitive = typeof Boolean | typeof Number | typeof String;
@@ -9,14 +14,17 @@ export module Primitive {
     }
 }
 
+/**
+ * A type that has a constructor.
+ */
 export type Class = new (...args: any) => any;
 
 /**
  * Takes a type T and returns:
- *  - the contained type if it is an Array
- *  - the instantiated type if it is a Class
- *  - the returned type if it is a Function
- *  - T if it is none of the options above
+ *  - the contained type if T is an Array, which is T[number]
+ *  - the instantiated type if T is a Class, which is InstanceType<T>
+ *  - the returned type if it T a Function, which is ReturnType<T>
+ *  - T if none of the above
  */
 export type Unbox<T>
     = T extends any[] ? T[number]
--- src/type.ts
+++ src/type.ts
@@ -1,7 +1,25 @@
 import { Class } from "./lang";
 
+/**
+ * The symbol that points to the type metadata.
+ */
 export const TypeSymbol: unique symbol = Symbol();
 
+/**
+ * The contract each entity class defined by you has to fulfill.
+ * 
+ * It requires your entity class to include the metadata that is required for type-vein to properly
+ * pick up your defined type. The metadata has to be stored at the unique symbol TypeSymbol, which
+ * can be easily implemented via Type.createMetadata():
+ * ```typescript
+ * class MyEntityType {
+ *    [TypeSymbol] = Type.createMetadata(MyEntityType);
+ * }
+ * ```
+ * 
+ * For more information about what the metadata is used for, check out the comments @ Type.Metadata.
+ * 
+ */
 export interface Type<T extends Class = Class> {
     [TypeSymbol]: Type.Metadata<T>;
 }
@@ -11,6 +29,13 @@ export module Type {
         return ((x || {}) as any as Type)[TypeSymbol]?.class instanceof Function;
     }
 
+    /**
+     * The metadata of a user-defined type of entity. It contains a reference to the class that produces the
+     * entity type when instantiated. The class reference is also used to uniquely identify a type.
+     * 
+     * Any part of type-vein that relies on fetching global data for a specific type of entity will
+     * use the class reference to look it up.
+     */
     export interface Metadata<T extends Class = Class> {
         class: T;
     }
