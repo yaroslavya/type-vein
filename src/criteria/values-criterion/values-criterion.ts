f217d0b2fda3fe08eb93271f4610197d88d482b9
Magna Marek <magna.marek@pbs.one>
2019-12-20 00:03:20 +0100

    [add] reduce()/reducer() @ values criterion non-functional as of yet #14

--- src/criteria/values-criterion/values-criterion.ts
+++ src/criteria/values-criterion/values-criterion.ts
@@ -19,4 +19,17 @@ export module ValuesCriterion {
     export function is(x?: any): x is ValuesCriterion {
         return operationsSet.has(x?.op);
     }
+
+    export function reduce(a: ValuesCriterion, b: ValuesCriterion): ValuesCriterion | null {
+        return reducer(a.op)(a, b);
+    }
+
+    export function reducer<OP extends ValuesCriterion["op"]>(op: OP): (a: Extract<ValuesCriterion, { op: OP }>, b: ValuesCriterion) => ValuesCriterion | null {
+        switch (op) {
+            case "intersect":
+            case "subset":
+            case "superset":
+            default: throw new Error(`no reducer known for operation '${op}'`);
+        }
+    }
 }
